6557691727d28efe11b400669e24603a8afaa738
Nickolays93 <nikolays93@ya.ru>
2018-12-11 10:08:55 +0400

    ;)

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -3,6 +3,7 @@
 const public_folder = '../public_html';
 
 const gulp = require('gulp'),
+    // gutil = require('gulp-util'),
     rename = require('gulp-rename'),
     // code
     rigger = require('gulp-rigger'),
@@ -10,13 +11,13 @@ const gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     // style
     sass = require('gulp-sass'),
-    // prefixer = require('gulp-autoprefixer'),
+    prefixer = require('gulp-autoprefixer'),
     cssmin = require('gulp-clean-css'),
     // image
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
     // watch changes
-    watch = require('gulp-watch'),
+    watcher = require('gulp-watch'),
     // server
     browserSync = require("browser-sync"),
     reload = browserSync.reload,
@@ -32,123 +33,116 @@ const dir = {
     base: './../public_html'
 }
 
-const srvConfig = {
+const syncConf = {
     server: {
         baseDir: dir.base
     },
-    tunnel: false,
+    tunnel: tunnel,
     host: 'localhost',
     port: 8080,
     logPrefix: 'gulp'
 };
 
-let path = {
-    src: {
-        // root only
-        code: dir.src + '*.html',
-        css: dir.src + 'template_styles.scss',
-        js: dir.src + 'assets/main.js',
-        img: dir.src + 'img/**/*.*',
-        font: dir.src + 'assets/fonts/*.*'
-    },
-    build: {
-        code: dir.build,
-        css: dir.build,
-        js: dir.build + 'assets/js/',
-        img: dir.build + 'img/',
-        font: dir.build + 'assets/fonts/'
-    },
-    watch: {
-        code: dir.src + '**/*.html',
-        css: [dir.src + 'template_styles.scss', dir.src + 'styles/**/*.scss'],
-        js: dir.src + 'assets/main.js',
-        img: dir.src + 'img/**/*.*',
-        font: dir.src + 'assets/fonts/*.*'
-    }
+
+/**
+ * Set patches
+ */
+let watch = {
+    code: [dir.src + '**/*.html', dir.src + '**/*.htm'],
+    css:  [dir.src + '**/*.scss'],
+    img:  [dir.src + 'img/**/*.*'],
+    js:   [dir.src + 'assets/**/*.js'],
+    font: [dir.src + 'assets/fonts/*.*']
 }
 
+// @todo think about push dir.src + '*.*' for htaccess, favicon..
+let src = watch;
+
+let build = {
+    code: dir.build,
+    css:  dir.build,
+    img:  dir.build + 'img/',
+    js:   dir.build + 'assets/',
+    font: dir.build + 'assets/fonts/'
+}
+
+let bs = {
+    css:  dir.src + 'styles/bootstrap.scss',
+    js:   dir.src + 'assets/bootstrap.js',
+    opts: dir.src + 'styles/_site-settings.scss',
+}
+
+// exclude snippets/includes
+src.code.push('!' + dir.src + 'include/**/*');
+src.css.push( '!' + dir.src + 'styles/**/*' );
+src.js.push( '!' + dir.src + 'assets/include/**/*', '!' + bs.js );
+
+// exclude boostrap
+watch.css.push('!' + bs.css);
+watch.js.push('!' + bs.js);
+
+/**
+ * Tasks
+ */
 gulp.task('build::code', function () {
-    gulp.src(path.src.code)
-        .pipe(rigger())
-    .pipe(gulp.dest(path.build.code))
+    gulp.src(src.code)
+    .pipe(rigger())
+    .pipe(gulp.dest(build.code))
         .pipe(reload(r));
 });
 
 gulp.task('build::style', function () {
-    gulp.src(path.src.css)
+    gulp.src(src.css)
         // .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        // .pipe(prefixer())
+        .pipe(prefixer())
         .pipe(cssmin())
+        // do not rename for proxy (bitrix for ex.)
+        // .pipe(rename({
+        //     suffix: '.min'
+        // }))
         // .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.build.css))
+    .pipe(gulp.dest(build.css))
         .pipe(reload(r));
 });
 
 gulp.task('build::js', function () {
-    gulp.src(path.src.js)
+    gulp.src(src.js)
         .pipe(rigger())
         // .pipe(sourcemaps.init())
-        // .pipe(uglify())
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         // .pipe(sourcemaps.write())
-    .pipe(gulp.dest(path.build.js))
+    .pipe(gulp.dest(build.js))
         .pipe(reload(r));
 });
 
 gulp.task('build::image', function () {
-    gulp.src(path.src.img)
+    gulp.src(src.img)
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
         }))
-    .pipe(gulp.dest(path.build.img))
+    .pipe(gulp.dest(build.img))
         .pipe(reload(r));
 });
 
 // move only
 gulp.task('build::font', function() {
-    gulp.src(path.src.font)
-        .pipe(gulp.dest(path.build.font));
-});
-
-gulp.task('watch', function() {
-    watch(path.watch.code, function(event, cb) {
-        gulp.start('build::code');
-    });
-
-    watch(path.watch.css, function(event, cb) {
-        gulp.start('build::style');
-    });
-
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('build::js');
-    });
-
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('build::image');
-    });
-
-    watch([path.watch.font], function(event, cb) {
-        gulp.start('build::font');
-    });
-
-    watch([dir.src + 'styles/bootstrap/**/*.scss', dir.src + 'styles/_site-settings.scss'], function(event, cb) {
-        gulp.start('vbuild::bootstrap-style');
-    });
-
-    watch([dir.src + 'assets/bootstrap.js'], function(event, cb) {
-        gulp.start('vbuild::bootstrap-script');
-    });
+    gulp.src(src.font)
+        .pipe(gulp.dest(build.font));
 });
 
 
 /**
- * build vendor packages (use after bower)
+ * Build bootstrap (use after bower)
  */
 gulp.task('vbuild::bootstrap-style', function () {
-    gulp.src(dir.src + 'styles/bootstrap/bootstrap.scss')
+    gulp.src( bs.css )
         // .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(cssmin()) // minify/uglify
@@ -161,7 +155,7 @@ gulp.task('vbuild::bootstrap-style', function () {
 });
 
 gulp.task('vbuild::bootstrap-script', function () {
-    gulp.src(dir.src + 'assets/bootstrap.js')
+    gulp.src( bs.js )
         .pipe(rigger())
         // .pipe(sourcemaps.init())
         .pipe(uglify())
@@ -169,41 +163,71 @@ gulp.task('vbuild::bootstrap-script', function () {
         .pipe(rename({
             suffix: '.min'
         }))
-    .pipe(gulp.dest(path.build.js))
+    .pipe(gulp.dest(build.js))
         .pipe(reload(r));
+});
+
+gulp.task('watch', function() {
+    watcher(watch.code, function(event, cb) {
+        gulp.start('build::code');
+    });
+
+    watcher(watch.css, function(event, cb) {
+        gulp.start('build::style');
+    });
+
+    watcher(watch.js, function(event, cb) {
+        gulp.start('build::js');
+    });
+
+    watcher(watch.img, function(event, cb) {
+        gulp.start('build::image');
+    });
+
+    watcher(watch.font, function(event, cb) {
+        gulp.start('build::font');
+    });
+
+    // Bootstrap
+    watcher([ bs.css, bs.opts ], function(event, cb) {
+        gulp.start('vbuild::bootstrap-style');
+    });
+
+    watcher([ bs.js ], function(event, cb) {
+        gulp.start('vbuild::bootstrap-script');
+    });
 });
 
 /**
  * Bower migrations
  */
-gulp.task('move::jquery', function () {
+gulp.task('move::assets', function () {
     gulp.src('bower_components/jquery/dist/jquery.min.js')
-        .pipe(gulp.dest(path.build.js))
-});
+        .pipe(gulp.dest(build.js))
 
-gulp.task('move::fancybox', function () {
-    gulp.src('bower_components/fancybox/dist/*.*')
-        .pipe(gulp.dest(path.build.js + 'fancybox/'))
-});
+    // gulp.src('bower_components/fancybox/dist/*.*')
+    //     .pipe(gulp.dest(build.code + 'assets/fancybox/'))
 
-gulp.task('move::slick', function () {
-    gulp.src('bower_components/slick-carousel/slick/**/*.*')
-        .pipe(gulp.dest(path.build.js + 'slick/'))
-});
+    // gulp.src('bower_components/slick-carousel/slick/**/*.*')
+    //     .pipe(gulp.dest(build.code + 'assets/slick/'))
 
-gulp.task('move::masonry', function () {
-    gulp.src('bower_components/masonry-layout/dist/**/*.*')
-        .pipe(gulp.dest(path.build.js + 'masonry/'))
+    // gulp.src('bower_components/masonry-layout/dist/**/*.*')
+    //     .pipe(gulp.dest(build.code + 'assets/masonry/'))
+
+    // gulp.src('bower_components/jquery-countTo/jquery.countTo.js')
+    //     .pipe(uglify())
+    //     .pipe(rename({
+    //         suffix: '.min'
+    //     }))
+    //     .pipe(gulp.dest(build.code + 'assets/count-to/'))
+
+    // gulp.src('bower_components/jquery-countTo/jquery.countTo.js')
+    //     .pipe(gulp.dest(build.code + 'assets/count-to/'))
 });
 
 gulp.task('webserver', function () {
-    browserSync(srvConfig);
+    browserSync(syncConf);
 });
-
-gulp.task('clean', function (cb) {
-    rimraf(dir.base, cb);
-});
-
 
 // build project
 gulp.task('build', [
@@ -214,27 +238,20 @@ gulp.task('build', [
     'build::image',
 ]);
 
-gulp.task('install', [
-    'vbuild',
-    'build::code',
-    'build::js',
-    'build::style',
-    'build::font',
-    'build::image',
-]);
-
-// move vendor packages
-gulp.task('vbuild', [
-    'move::jquery',
-    'move::fancybox',
-    'move::slick',
-    'move::masonry',
-]);
-
 // build bootstrap
 gulp.task('vbuild::bootstrap', [
     'vbuild::bootstrap-style',
     'vbuild::bootstrap-script',
 ]);
+
+
+gulp.task('install', [
+    'move::assets',
+    // 'build',
+]);
+
+gulp.task('clean', function (cb) {
+    rimraf(dir.base, cb);
+});
 
 gulp.task('default', ['watch', 'vbuild::bootstrap', 'build', 'webserver']);
--- source/assets/main.js
+++ source/assets/main.js
@@ -1,20 +1,13 @@
-const Settings = {
-  is_mobile: false,
-  sticky: false, // || 'everywhere' || 'mobile',
-  stickySelector: false, // || '#main.navbar',
-  wow: false, // || 'everywhere' || 'desktop',
-  appearJs: false, // || true,
-  countTo: false, // || '.counter',
-  popovers: false, // || '[data-toggle="popover"]',
-  fancybox: false, // || '.zoom',
-  masonry: false, // || '.grid',
-  masonryOpts: {
-    itemSelector: '.grid-item',
-    columnWidth: 200,
-  },
-}
-
 jQuery(document).ready(function($) {
+    var Settings = {
+        is_mobile: false,
+        sticky: false, // || 'everywhere' || 'mobile',
+        stickySelector: false, // || '#main.navbar',
+        wow: false, // || 'everywhere' || 'desktop',
+        appearJs: false, // || true,
+        countTo: false, // || '.counter',
+    }
+
     // sticky
     if( Settings.stickySelector ) {
         if( 'mobile' == Settings.sticky && Settings.is_mobile || Settings.sticky == 'everywhere' ) {
@@ -51,82 +44,6 @@ jQuery(document).ready(function($) {
         $( Settings.countTo ).countTo();
     }
 
-    if( Settings.popovers ) {
-        var $popovers = $(Settings.popovers);
-
-        function loadPopoverContent( $self, $target ) {
-            var $clone = $target.clone(1).css('display', 'block');
-            var href = $self.attr('href');
-
-            if( href.length > 1 ) {
-                $target.remove();
-
-                $.ajax({
-                    type: 'POST',
-                    url: href,
-                    data: {is_ajax : 'Y'},
-                    success: function(response) {
-                        $clone.append(response);
-                    }
-                });
-            }
-
-            return $clone;
-        }
-
-        $popovers.each(function(index, el) {
-            var $self = $(this);
-
-            $(this).on('click', function(event) {
-                event.preventDefault();
-            });
-
-            var popProps = {
-                placement: $self.data('placement') || 'bottom'
-            };
-
-            var target = $self.data('content-target') || '';
-            var $target = $( target );
-
-            if( $target.length ) {
-                popProps = {
-                    html: true,
-                    content: function() {
-                        return loadPopoverContent($self, $target);
-                    },
-                }
-            }
-
-            var $popover = $self.popover(popProps);
-        });
-
-        /**
-         * Close button in popover content
-         */
-         $(document).on("click", ".popover .close" , function() {
-            $(this).parents(".popover").popover('hide');
-        });
-
-        /**
-         * Hide all the Popovers if clicked on button or inside popover
-         */
-         $(document).on("click", function (e) {
-            var $target = $(e.target);
-            var isPopover = $target.is('[data-toggle="popover"]'),
-            inPopover = $target.closest('.popover').length > 0
-
-            if (!isPopover && !inPopover) $popovers.popover('hide');
-        });
-    }
-
-    if( Settings.fancybox ) {
-        $( Settings.fancybox ).fancybox();
-    }
-
-    if( Settings.masonry ) {
-        $( Settings.masonry ).masonry( Settings.masonryOpts );
-    }
-
     // $('.responsive-slider').slick({
     //     autoplay: true,
     //     autoplaySpeed: 4000,
@@ -134,31 +51,6 @@ jQuery(document).ready(function($) {
     //     infinite: false,
     //     speed: 300,
     //     slidesToShow: 4,
-    //     slidesToScroll: 4,
-    //     responsive: [
-    //     {
-    //         breakpoint: 1024,
-    //         settings: {
-    //             slidesToShow: 3,
-    //             slidesToScroll: 3,
-    //             infinite: true,
-    //             dots: true
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 600,
-    //         settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 2
-    //         }
-    //     },
-    //     {
-    //         breakpoint: 480,
-    //         settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1
-    //         }
-    //     }
-    //     ]
+    //     slidesToScroll: 4
     // });
 });
