381eca16eaab2e5002efa2eff95cae430ab6b0f3
Nickolays93 <nikolays93@ya.ru>
2019-01-22 11:37:26 +0400

    add hamburger

--- public_html/assets/raw/bootstrap.js
+++ public_html/assets/raw/bootstrap.js
@@ -1,15 +1,15 @@
 // required `bower install popper.js`
-// = ../../../node/bower_components/popper.js/dist/umd/popper.js
+//= ../../../node/bower_components/popper.js/dist/umd/popper.js
 
-// = ../../../node/bower_components/bootstrap/js/dist/util.js
+//= ../../../node/bower_components/bootstrap/js/dist/util.js
 // ../../node/bower_components/bootstrap/js/dist/alert.js
 // ../../node/bower_components/bootstrap/js/dist/button.js
 // ../../node/bower_components/bootstrap/js/dist/carousel.js
-// = ../../../node/bower_components/bootstrap/js/dist/collapse.js
+//= ../../../node/bower_components/bootstrap/js/dist/collapse.js
 // ../../node/bower_components/bootstrap/js/dist/modal.js
-// = ../../../node/bower_components/bootstrap/js/dist/scrollspy.js
-// = ../../../node/bower_components/bootstrap/js/dist/tab.js
+//= ../../../node/bower_components/bootstrap/js/dist/scrollspy.js
+//= ../../../node/bower_components/bootstrap/js/dist/tab.js
 // required popper.js
-// = ../../../node/bower_components/bootstrap/js/dist/dropdown.js
+//= ../../../node/bower_components/bootstrap/js/dist/dropdown.js
 // ../../node/bower_components/bootstrap/js/dist/tooltip.js
 // ../../node/bower_components/bootstrap/js/dist/popover.js
