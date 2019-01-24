c7fd3d67e490b399ad73e6e41e891dc153ae613c
nikolays93 <nikolays93@ya.ru>
2019-01-25 00:47:31 +0400

    It seems to work :zzz:

--- clone-submodule.sh
+++ clone-submodule.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
+
+git pull
 git submodule init
 git submodule update
-cd public_html
-git checkout origin/master
+cd  public_html
+git checkout master
+git pull origin master
 
 read -n 1 -s -r -p "Press any key to exit"
