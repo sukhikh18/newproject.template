6aa209a5ca70459e37fc4aad3b59225cacbc94da
NikolayS93 <nikolays93@ya.ru>
2019-04-29 23:46:44 +0400

    init dev

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 ﻿# New project Boilerplate
 
-## Особенности
-- автоматическая перезагрузка страницы в браузере с использованием [Browsersync](https://www.browsersync.io/);
-- использование препроцессора [SCSS](https://sass-lang.com/);
-
 ## Установка
 1. Установите [node.js](https://nodejs.org/en/download/).
 2. Установите [Yarn](https://yarnpkg.com/en/docs/install/).
@@ -16,26 +12,6 @@
 * Введите команду ```yarn``` для установки/сборки проекта;
 * Введите ```yarn run dev``` или ```gulp``` для разработки;
 * Введите ```yarn run prod``` или ```gulp build --production``` для завершения разработки;
-
-## Плагины
-* [browser-sync](https://browsersync.io/docs/gulp) - живая перезагрузка веб-страницы при внесении изменений в файлы вашего проекта;
-* [gulp-autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer) — автоматически расставляет вендорные префиксы в CSS в соответствии с сервисом [Can I Use](https://caniuse.com/);
-* [gulp-uglify](https://www.npmjs.com/package/gulp-uglify) — минификация JS-файлов;
-* [gulp-sass](https://www.npmjs.com/package/gulp-sass) — компиляция SCSS в CSS;
-* [gulp-group-css-media-queries](https://www.npmjs.com/package/gulp-group-css-media-queries) - группировка ```@media```;
-* [gulp-clean-css](https://www.npmjs.com/package/gulp-clean-css) — минификация CSS-файлов;
-* [gulp-sourcemaps](https://www.npmjs.com/package/gulp-sourcemaps) - карта стилей;
-* [gulp-rename](https://www.npmjs.com/package/gulp-rename) — переименование файлов, добавление суффиксов и префиксов (например, добавление суффикса ```.min``` к минифицированным файлам);
-* [gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin) — сжатие изображений PNG, JPG, GIF и SVG (включая дополнительные плагины для оптимизации);
-* [gulp-if](https://www.npmjs.com/package/gulp-if) - запуск заданий только тогда, когда это нужно;
-* [gulp-replace](https://www.npmjs.com/package/gulp-replace) - замена строк;
-* [gulp-rigger](https://www.npmjs.com/package/gulp-rigger) - позволяет вставлять содержимое из отдельных файлов в основной;
-* [gulp-plumber](https://www.npmjs.com/package/gulp-plumber) — оповещения в командной строке (например, ошибки в SCSS/Sass);
-* [gulp-debug](https://www.npmjs.com/package/gulp-debug) — отладка в терминале;
-* [gulp-watch](https://www.npmjs.com/package/gulp-watch) — отслеживание изменений в ваших файлах проекта;
-* [gulp-clean](https://www.npmjs.com/package/gulp-clean) — удаление файлов и папок;
-* [gulp-newer](https://www.npmjs.com/package/gulp-newer) - пропуск повторной обработки;
-* [yargs](https://www.npmjs.com/package/yargs) - получение аргументов командной строки в Node.js.
 
 ___
 За основу взят сборщик [gulp-scss-starter](https://github.com/andreyalexeich/gulp-scss-starter) от [Андрея Горохова](https://github.com/andreyalexeich)
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,21 @@
 "use strict";
 
 /** @type {String} For use proxy */
-global.domain = ''; // wordpress.cms
+global.domain = '';
 
-/** @type {Boolean} Need proxy for multiple devices */
-global.tunnel = false;
+global.serverCfg = {
+	port: 9000,
+	/** @type {Boolean} Need proxy for multiple devices */
+	tunnel: false,
+	notify: false
+}
 
-/** @type {String} Path to the template directory */
-global.dir = './public_html/'; // wp-content/themes/project/
+/** @global {String} Path to the root directory */
+global.dir   = './public_html/';
+global.dist = './public_html/';
 
+// import webpack from "webpack";
+// import webpackStream from "webpack-stream";
 import { src, dest, watch, parallel, series } from "gulp";
 import gulpif from "gulp-if";
 import browsersync from "browser-sync";
@@ -16,76 +23,45 @@ import autoprefixer from "gulp-autoprefixer";
 import uglify from "gulp-uglify";
 import sass from "gulp-sass";
 import groupmediaqueries from "gulp-group-css-media-queries";
+// import postcss from "gulp-postcss";
+// import mqpacker from "css-mqpacker";
+// import sortCSSmq from "sort-css-media-queries";
 import mincss from "gulp-clean-css";
 import sourcemaps from "gulp-sourcemaps";
 import rename from "gulp-rename";
-import newer from "gulp-newer";
-import imagemin from "gulp-imagemin";
-import imageminPngquant from "imagemin-pngquant";
-import imageminZopfli from "imagemin-zopfli";
-import imageminMozjpeg from "imagemin-mozjpeg";
-import imageminGiflossy from "imagemin-giflossy";
+import favicons from "gulp-favicons";
+import svgSprite from "gulp-svg-sprite";
 import replace from "gulp-replace";
 import rigger from "gulp-rigger";
 import plumber from "gulp-plumber";
 import debug from "gulp-debug";
 import clean from "gulp-clean";
 import yargs from "yargs";
+import smartgrid from "smart-grid";
 
-const argv = yargs.argv;
+import newer from "gulp-newer";
+import imagemin from "gulp-imagemin";
+import imageminPngquant from "imagemin-pngquant";
+import imageminZopfli from "imagemin-zopfli";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminGiflossy from "imagemin-giflossy";
+// import imageminWebp from "imagemin-webp";
+// import webp from "gulp-webp";
 
 global.paths = {};
+global.production = !!yargs.argv.production;
 
-global.raw    = '';
-global.assets = '';
-global.scss   = '';
-global.js     = '';
-global.img    = '';
+global.assets = 'assets/';
+global.scss   = 'styles/';
+global.js     = 'assets/';
+global.img    = 'img/';
 
-global.additionalTasks = [];
-global.additionalAssetsTasks = [];
-global.additionalWatch = function() {}
+let { additionalAssetsTasks } = require(dir + "config");
 
-/** Path to the template configuration */
-// @todo How can i use to same as import?
-// import getPaths from global.dir + "config";
-
-global.production = !!argv.production;
-
-const { additionalAssetsTasks, additionalTasks, additionalWatch } = require(dir + "config");
-
-// @todo fix it! (not worked)
-// Do not recursive overdose
-paths.watch.html.push('!*.html');
-
-export const server = () => {
-	let bsOpts = {
-		port: 9000,
-		tunnel: tunnel,
-		notify: false
-	}
-
-	if( '' === domain ) {
-		bsOpts.server = {baseDir: paths.build.general};
-	}
-	else {
-		bsOpts.proxy = domain;
-	}
-
-	browsersync.init(bsOpts);
-};
-
-export const watchCode = () => {
-	watch(paths.watch.html, html);
-	watch(paths.watch.styles, styles);
-	watch(paths.watch.scripts, scripts);
-	watch(paths.src.images, images);
-
-    additionalWatch();
-};
+if( !additionalAssetsTasks ) additionalAssetsTasks = [];
 
 // @warning do not change .html files (use htm) !! recursive updates !!
-export const html = () => src(paths.src.html, { allowEmpty: true })
+export const html = () => src( paths.src.html, { allowEmpty: true })
 	.pipe(rigger())
 	.pipe(gulpif(production, replace("template_styles.css", "template_styles.min.css")))
 	.pipe(gulpif(production, replace("main.js", "main.min.js")))
@@ -98,44 +74,66 @@ export const html = () => src(paths.src.html, { allowEmpty: true })
 	}))
 	.on("end", browsersync.reload);
 
-export const styles = () => src(paths.src.styles)
+const defAutoPrefArgs = {
+    browsers: ["last 12 versions", "> 1%", "ie 8", "ie 7"]
+};
+
+const defMinCssArgs = {
+    compatibility: "ie8",
+    level: {
+        1: {
+            specialComments: 0,
+            removeEmpty: true,
+            removeWhitespace: true
+        },
+        2: {
+            mergeMedia: true,
+            removeEmpty: true,
+            removeDuplicateFontRules: true,
+            removeDuplicateMediaBlocks: true,
+            removeDuplicateRules: true,
+            removeUnusedAtRules: false
+        }
+    },
+    rebase: false
+};
+
+export const styles = () => src(paths.src.styles, { allowEmpty: true })
 	.pipe(plumber())
-	.pipe(gulpif(!production, sourcemaps.init()))
+	// .pipe(gulpif(!production, sourcemaps.init()))
 	.pipe(sass())
 	.pipe(groupmediaqueries())
-	.pipe(gulpif(production, autoprefixer({
-		browsers: ["last 12 versions", "> 1%", "ie 8", "ie 7"]
-	})))
+	.pipe(gulpif(production, autoprefixer(defAutoPrefArgs)))
 	.pipe(gulpif(!production && '' != domain, browsersync.stream()))
-	.pipe(gulpif(production, mincss({
-		compatibility: "ie8", level: {
-			1: {
-				specialComments: 0,
-				removeEmpty: true,
-				removeWhitespace: true
-			},
-			2: {
-				mergeMedia: true,
-				removeEmpty: true,
-				removeDuplicateFontRules: true,
-				removeDuplicateMediaBlocks: true,
-				removeDuplicateRules: true,
-				removeUnusedAtRules: false
-			}
-		}
-	})))
-	.pipe(gulpif(production, rename({
-		suffix: ".min"
-	})))
+	.pipe(gulpif(production, mincss(defMinCssArgs)))
+	.pipe(gulpif(production, rename({ suffix: ".min" })))
 	.pipe(plumber.stop())
-	.pipe(gulpif(!production, sourcemaps.write("./assets/maps/")))
+	// .pipe(gulpif(!production, sourcemaps.write("./assets/maps/")))
 	.pipe(dest(paths.build.styles))
 	.pipe(debug({
 		"title": "CSS files"
 	}))
 	.on("end", () => production || '' == domain ? browsersync.reload : null);
 
-export const scripts = () => src(paths.src.scripts)
+export const stylesAssets = () => src([dir + assets + '*.scss', '!' + dir + assets + '_*.scss'], { allowEmpty: true })
+	.pipe(plumber())
+	// .pipe(gulpif(!production, sourcemaps.init()))
+	.pipe(sass())
+	// .pipe(groupmediaqueries())
+	.pipe(gulpif(production, autoprefixer(defAutoPrefArgs)))
+    .pipe(gulpif(production, mincss(defMinCssArgs)))
+	.pipe(gulpif(production, rename({
+		suffix: ".min"
+	})))
+	.pipe(plumber.stop())
+	// .pipe(gulpif(!production, sourcemaps.write("./assets/maps/")))
+	.pipe(dest(dist + assets))
+	.pipe(debug({
+		"title": "CSS files"
+	}))
+	.on("end", () => browsersync.reload);
+
+export const scripts = () => src(paths.src.scripts, { allowEmpty: true })
 	.pipe(plumber())
 	.pipe(rigger())
 	.pipe(gulpif(!production, sourcemaps.init()))
@@ -150,7 +148,7 @@ export const scripts = () => src(paths.src.scripts)
 	}))
 	.on("end", browsersync.reload);
 
-export const images = () => src(paths.src.images)
+export const images = () => src(paths.src.images, { allowEmpty: true })
 	.pipe(newer(paths.build.images))
 	.pipe(gulpif(production, imagemin([
 		imageminGiflossy({
@@ -188,17 +186,37 @@ export const images = () => src(paths.src.images)
 	}))
 	.on("end", browsersync.reload);
 
+export const server = () => {
+	if( '' !== domain ) {
+		serverCfg.proxy = domain;
+	}
+	else {
+		serverCfg.server = {baseDir: dist};
+	}
+
+	browsersync.init(serverCfg);
+};
+
+export const watchCode = () => {
+	watch(paths.watch.html,    html);
+	watch(paths.watch.styles,  styles);
+	watch(paths.watch.scripts, scripts);
+	watch(paths.watch.images,  images);
+
+    // if(additionalWatch) additionalWatch();
+};
+
 
 export const common = parallel(html, styles, scripts, images);
 
 /**
  * Move assets, build and stop
  */
-export const build = series(additionalAssetsTasks || global.additionalAssetsTasks, additionalTasks || global.additionalTasks, common);
+export const build = series(additionalAssetsTasks, stylesAssets, common);
 
 /**
  * Build and start server with watcher
  */
-export const run = series(additionalTasks || global.additionalTasks, common, parallel(watchCode || global.watchCode, server));
+export const run = series(stylesAssets, common, parallel(watchCode, server));
 
 export default run;
