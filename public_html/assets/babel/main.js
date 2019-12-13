cac51da172769f8103b850682d67d362501e5f56
nikolays93 <nikolays93@ya.ru>
2019-12-13 11:29:10 +0400

    Start page upgrade #2.

--- public_html/assets/babel/main.js
+++ public_html/assets/babel/main.js
@@ -1,10 +1,16 @@
 // for smooth scrool to object
 import scrollTo from "./parts/_scrollTo.js";
+import preloader from "./parts/_preloader.js";
 
 jQuery(document).ready(function($) {
     $(document).on('click', '[href^="#"]', function(event) {
         event.preventDefault();
         scrollTo( this.getAttribute("href") );
+    });
+
+    new Cleave('[type="tel"]', {
+        phone: true,
+        phoneRegionCode: 'RU'
     });
 
     /**
@@ -17,4 +23,31 @@ jQuery(document).ready(function($) {
     //     },
     //     offset: 50
     // });
+
+    /**
+     * Example form submit event.
+     */
+    if( typeof($.fancybox) ) {
+        $('form').on('submit', function(event) {
+            event.preventDefault();
+            preloader.show();
+
+            // Disable retry by 120 seconds
+            const $submit = $(this).find('button');
+            $submit.attr('disabled', 'disabled');
+            setTimeout(function() {
+                $submit.removeAttr('disabled');
+            }, 120000);
+
+            // Show success
+            setTimeout(function() {
+                preloader.hide();
+
+                $.fancybox.open({
+                    content  : '<h1>Отлично!</h1><p>Ваша заявка принята, ожидайте звонка.</p>',
+                    type     : 'html',
+                });
+            }, 5000);
+        });
+    }
 });
