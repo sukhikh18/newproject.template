33cd9247d96085cf777901b7ac79ef333a92b485
nikolays93 <nikolays93@ya.ru>
2020-05-17 15:42:44 +0400

    Move js to another folder

--- public_html/assets/.source/parts/_scrollTo.js
+++ public_html/assets/.source/parts/_scrollTo.js
@@ -1,14 +1,13 @@
-
-window.scrollTo = (target, topOffset = 30, delay = 400) => {
-	if( !target || target.length <= 1 ) return false
+const scrollTo = (target, topOffset = 30, delay = 400) => {
+    if( !target || target.length <= 1 ) return false
     let $target;
 
-	if( target instanceof jQuery ) {
+    if( target instanceof jQuery ) {
         $target = target.first()
-	}
-	else {
-		$target = $(target).length ? $(target).first() : $('a[name='+target.slice(1)+']').first()
-	}
+    }
+    else {
+        $target = $(target).length ? $(target).first() : $('a[name='+target.slice(1)+']').first()
+    }
 
     if( $target.offset().top ) {
         // for call from dropdown
